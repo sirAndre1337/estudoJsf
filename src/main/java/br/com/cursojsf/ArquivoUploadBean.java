c63e2f145bd243a4a1e4f54bec18cc7211eaee67
Andre Luis <andrelluis17@hotmail.com>
2021-09-09 18:39:52 -0300

    Lendo os dados de um arquivo csv e gravando no sistema

--- src/main/java/br/com/cursojsf/ArquivoUploadBean.java
+++ src/main/java/br/com/cursojsf/ArquivoUploadBean.java
@@ -12,6 +12,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.Part;
 
+import br.com.dao.DaoGeneric;
 import br.com.entidades.ArquivoUpload;
 import br.com.entidades.Pessoa;
 import br.com.repository.IDaoArquivoUpload;
@@ -27,6 +28,9 @@ public class ArquivoUploadBean implements Serializable{
 	
 	@Inject
 	private IDaoArquivoUpload iDaoArquivoUpload;
+	
+	@Inject
+	private DaoGeneric<Pessoa> dao;
 	
 	public String salvar() throws IOException {
 		
@@ -47,6 +51,42 @@ public class ArquivoUploadBean implements Serializable{
 		
 		
 		return "";
+	}
+	
+	public String upload() throws IOException {
+		
+		// Lendo arquivo em CSV --------
+		
+				Scanner scanner = new Scanner(arquivo.getInputStream() , "UTF-8");
+				scanner.useDelimiter(",");
+				
+				String linha;
+				int aux = 0;
+				
+				while (scanner.hasNext() && aux == 0) {
+					linha = scanner.nextLine();
+					
+					if (linha != null && !linha.trim().isEmpty()) {
+						
+						if (!linha.equalsIgnoreCase(";")) {
+						String[] dados = linha.split("\\;"); // onde tem a virgula ele quebra
+						System.out.println("Nome :" + dados[0] + " Email : "+ dados[1]);
+						
+						Pessoa p = new Pessoa();
+						p.setNome(dados[0]);
+						p.setLogin(dados[1]);
+						dao.atualizarOuSalva(p);
+						
+						} else {
+							aux = 1;
+						}
+						
+					}
+					
+				}
+				
+				return "";
+		
 	}
 	
 	// Pega o arquivo e transforma em byte
